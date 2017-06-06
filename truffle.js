c614c4461f38b4ad4b42f5ab80a9bd2200acda9e
Nadav Hollander <nadavhollander@Nadavs-MacBook-Pro.local>
2017-06-06 10:48:29 -0700

    added solc build processes

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+const exec = require('child_process').exec;
+
 module.exports = {
   networks: {
     development: {
@@ -5,5 +7,19 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  build: function(options, callback) {
+     // Do something when a build is required. `options` contains these values:
+     //
+     // working_directory: root location of the project
+     // contracts_directory: root directory of .sol files
+     // destination_directory: directory where truffle expects the built assets (important for `truffle serve`)
+     const solc_compile = "docker run -v " + options.working_directory + ":/DharmaLoanStandard \
+       ethereum/solc:stable -o /DharmaLoanStandard/dist --bin --abi \
+       /DharmaLoanStandard/contracts/Loan.sol"
+
+     exec(solc_compile, function(error, stdout, stderr) {
+       callback(error);
+     })
   }
 };
