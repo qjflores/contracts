e7e975b06b0f586e4e3cc66e3cc9b5351650d4ca
Nadav Hollander <nadav@dharma.io>
2017-10-06 16:09:49 -0700

    clean up directory

--- truffle.js
+++ truffle.js
@@ -8,25 +8,21 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-    dharma: {
+    private: {
       host: "localhost",
       port: 8546,
-      from: '0x8886e57e0484b7b5e119f8cd2dbb50e5be38d9b9',
       network_id: 40734,
       gas: 3000000
     },
     kovan: {
       host: "localhost",
       port: 8545,
-      from: '0x0d42fe90615b452b58e04cb096c335abdd4211c8',
       network_id: 42
     },
     ropsten: {
       host: "localhost",
       port: 8545,
-      from: '0x9392208ae53263ff2769abf4c030490c5aa4c845',
       network_id: 3,
-      gas: 3000000
     }
   }
 };
